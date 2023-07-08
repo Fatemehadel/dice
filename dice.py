c4e2f1feb06502b472b0a821501e0e4b6daabdc6
Fatemehadel <fatemehadel2674@gmail.com>
2023-07-09 01:30:06 +0430

    first code commit

---
+++ dice.py
@@ -0,0 +1,112 @@
+from random import randint
+
+a = input("press Y to continue: ")
+num = randint(1,21)
+if a =="y":
+    if (num == 1):
+        print("[       ] [       ]")
+        print("[   0   ] [   0   ]")
+        print("[       ] [       ]")
+
+    if (num == 2):
+        print("[       ] [   0   ]")
+        print("[   0   ] [       ]")
+        print("[       ] [   0   ]")  
+
+    if (num == 3):
+        print("[       ] [   0   ]")
+        print("[   0   ] [   0   ]")
+        print("[       ] [   0   ]") 
+
+    if (num == 4):
+        print("[       ] [ 0   0 ]")
+        print("[   0   ] [       ]")
+        print("[       ] [ 0   0 ]") 
+
+    if (num == 5):
+        print("[       ] [ 0   0 ]")
+        print("[   0   ] [   0   ]")
+        print("[       ] [ 0   0 ]")
+
+    if (num == 6):
+        print("[       ] [ 0   0 ]")
+        print("[   0   ] [ 0   0 ]")
+        print("[       ] [ 0   0 ]")
+
+    if (num == 7):
+        print("[   0   ] [   0   ]")
+        print("[       ] [       ]")
+        print("[   0   ] [   0   ]")
+
+    if (num == 8):
+        print("[   0   ] [   0   ]")
+        print("[       ] [   0   ]")
+        print("[   0   ] [   0   ]") 
+
+    if (num == 9):
+        print("[   0   ] [ 0   0 ]")
+        print("[       ] [       ]")
+        print("[   0   ] [ 0   0 ]")   
+
+    if (num == 10):
+        print("[   0   ] [ 0   0 ]")
+        print("[       ] [   0   ]")
+        print("[   0   ] [ 0   0 ]") 
+
+    if (num == 11):
+        print("[   0   ] [ 0   0 ]")
+        print("[       ] [ 0   0 ]")
+        print("[   0   ] [ 0   0 ]") 
+
+    if (num == 12):
+        print("[   0   ] [   0   ]")
+        print("[   0   ] [   0   ]")
+        print("[   0   ] [   0   ]") 
+
+    if (num == 13):
+        print("[   0   ] [ 0   0 ]")
+        print("[   0   ] [       ]")
+        print("[   0   ] [ 0   0 ]") 
+
+    if (num == 14):
+        print("[   0   ] [ 0   0 ]")
+        print("[   0   ] [   0   ]")
+        print("[   0   ] [ 0   0 ]")
+
+    if (num == 15):
+        print("[   0   ] [ 0   0 ]")
+        print("[   0   ] [ 0   0 ]")
+        print("[   0   ] [ 0   0 ]")
+
+    if (num == 16):
+        print("[ 0   0 ] [ 0   0 ]")
+        print("[       ] [       ]")
+        print("[ 0   0 ] [ 0   0 ]")
+
+    if (num == 17):
+        print("[ 0   0 ] [ 0   0 ]")
+        print("[       ] [   0   ]")
+        print("[ 0   0 ] [ 0   0 ]")
+
+    if (num == 18):
+        print("[ 0   0 ] [ 0   0 ]")
+        print("[       ] [ 0   0 ]")
+        print("[ 0   0 ] [ 0   0 ]")
+
+    if (num == 19):
+        print("[ 0   0 ] [ 0   0 ]")
+        print("[   0   ] [   0   ]")
+        print("[ 0   0 ] [ 0   0 ]")
+
+    if (num == 20):
+        print("[ 0   0 ] [ 0   0 ]")
+        print("[   0   ] [ 0   0 ]")
+        print("[ 0   0 ] [ 0   0 ]")
+
+    if (num == 21):
+        print("[ 0   0 ] [ 0   0 ]")
+        print("[ 0   0 ] [ 0   0 ]")
+        print("[ 0   0 ] [ 0   0 ]")
+
+print("\n") 
+
